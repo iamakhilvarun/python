3e6bf6548a5211a3a19892b7948ad0c312883a10
iamakhilvarun <iamakhilvarun@gmail.com>
2026-07-17 21:26:14 +0530

    challenge completed

--- Modules/challenge.py
+++ Modules/challenge.py
@@ -9,13 +9,16 @@ except ImportError:
 zones = ("Europe/Paris", "Asia/Kolkata", "Europe/London", "Africa/Nairobi")
 
 # Current UTC time
-utc_now = datetime.now(timezone.utc)
-
+# utc_now = datetime.now(timezone.utc)
+local_now = datetime.now()
+local_now = local_now.replace(microsecond=0)
 for zone in zones:
     tz = zoneinfo.ZoneInfo(zone)
-    required_time = utc_now.astimezone(tz)
-
+    # required_time = utc_now.astimezone(tz)
+    # required_time = datetime.now(tz=tz)
+    required_time = local_now.astimezone(tz)
     # Get city name
-    city = zone.split("/")[-1]
+    city = zone.split("/")[1]
 
-    print(f"The time in {city} is {required_time}")
+    print(f"The time in {city} is {required_time} {required_time.tzname( )}")
+    # print(f"The time in {city} is {required_time.strftime("%m/%d/%Y %H:%M:%S %z %Z")}")
