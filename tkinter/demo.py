a8d8a3d91702a7fe824360d9943130f75732d804
AKHIL KUMAR VARUN <iamakhilvarun@gmail.com>
2026-07-18 10:32:06 +0530

    making a window and buttons

--- tkinter/demo.py
+++ tkinter/demo.py
@@ -1,6 +1,31 @@
-import tkinter as tk
+try:
+    import tkinter
+except ImportError:
+     import Tkinter as tkinter # not nessassry to write ==> for python 2
 
-print(tkinter,TkVersion)
-print(tkinter,TclVersion)
+mainwindow= tkinter.Tk()
 
-tkinter._test()
+mainwindow.title("Hello world")
+mainwindow.geometry('640x480+600+200')
+
+label=tkinter.Label(mainwindow,text="Hello world")
+label.pack(side="top")
+
+leftframe=tkinter.Frame(mainwindow)
+leftframe.pack(side='left',anchor='n',fill=tkinter.Y,expand=False)
+
+canvas=tkinter.Canvas(leftframe,relief='raised',borderwidth=1)
+canvas.pack(side='left',anchor='n')
+
+rightframe=tkinter.Frame(mainwindow)
+rightframe.pack(side='right',anchor='n',expand=True)
+
+button1=tkinter.Button(rightframe,text='button 1')
+button2=tkinter.Button(rightframe,text='button 2')
+button3=tkinter.Button(rightframe,text='button 3')
+
+button1.pack(side='top')
+button2.pack(side='top')
+button3.pack(side='top')
+
+mainwindow.mainloop()
