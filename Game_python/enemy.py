c34d1cdb017cda2a0e51dad6691e73bd80f5ee40
iamakhilvarun <iamakhilvarun@gmail.com>
2026-08-02 17:17:28 +0530

    using inhertance in GAME

--- Game_python/enemy.py
+++ Game_python/enemy.py
@@ -1,20 +1,64 @@
+import random
+
 class Enemy:
-    def __init__(self,name='Enemy',hit_points=0,lives=1):
-        self.name=name
-        self.hit_points=hit_points
-        self.lives=lives
+    def __init__(self, name="Enemy", hit_points=0, lives=1):
+        self._name = name
+        self._hit_points = hit_points
+        self._lives = lives
+        self._alive = True
 
-    def take_damage(self,damage):
-        remaining_points=self.hit_points - damage
-        if remaining_points >=0:
-            self.hit_points=remaining_points
-            print(f"I took {damage} points damage and have {self.hit_points} left")
+    def take_damage(self, damage):
+        remaining_points = self._hit_points - damage
+        if remaining_points >= 0:
+            self._hit_points = remaining_points
+            print(f"I took {damage} points damage and have {self._hit_points} left")
         else:
-            self.lives-=1
-
+            self._lives -= 1
+            if self._lives > 0:
+                print(f"{self._name} lost a life")
+            else:
+                print(f"{self._name} is dead")
+                self._alive = False
 
     def __str__(self):
-        return "Name: {0.name}, Lives: {0.lives}, Hit points: {0.hit_points}".format(self)
+        return "Name: {0._name}, Lives: {0._lives}, Hit points: {0._hit_points}".format(
+            self
+        )
+
 
 class Troll(Enemy):
-    pass
+    def __init__(self, name):
+        # super(Troll,self).__init__(name=name,lives=1,hit_points=23) --> this is used in python 2
+        super().__init__(name=name, lives=1, hit_points=23)
+
+    def grunt(self):
+        print(f"Me {self._name}. {self._name} stomp you")
+
+
+class Vampire(Enemy):# subclass of Enemy(superclass)
+    def __init__(self, name):
+        super().__init__(name=name, lives=4, hit_points=60)
+
+
+    def dodges(self):
+        if random.randint(1,3)==3:
+            print(f"****** {self._name} doges ******")
+            return True
+        else:
+            return False
+
+    def take_damage(self,damage):
+        if not self.dodges():
+            super().take_damage(damage=damage)
+
+    
+class Vampire_king(Vampire):
+    def __init__(self,name):
+        super().__init__(name)
+
+        self._lives=1
+        self._hit_points=140    
+
+    def take_damage(self,damage):
+        damage //= 4 # reduce damage by quarter half
+        super().take_damage(damage)
--- Game_python/main.py
+++ Game_python/main.py
@@ -1,11 +1,44 @@
-from enemy import Enemy,Troll
-
-ugly_troll=Troll()
+from enemy import Enemy, Troll,Vampire,Vampire_king
+ugly_troll = Troll("pug")
 print(f"ugly troll - {ugly_troll}")
 
-another_troll=Troll("ug",18,1)
+another_troll = Troll("ug")
 print(f"Another troll - {another_troll}")
 
 
-brother_troll=Enemy("big",32)
+brother_troll =Troll("nig")
 print(brother_troll)
+
+ugly_troll.grunt()
+another_troll.grunt()
+brother_troll.grunt()
+
+
+monster=Troll("Basic Enemy")
+monster.grunt()
+
+monster.take_damage(10)
+print(monster)
+
+vamp=Vampire("Dracula")
+print(vamp)
+
+vamp.take_damage(23)
+print(vamp)
+
+print("--------------------------")
+
+# while vamp.alive:
+#     if not vamp.dodges():
+#         vamp.take_damage(1)
+        # print(vamp)
+
+vamp._lives=1
+vamp._hit_points=1
+print(vamp)
+
+Boss=Vampire_king("rimuru")
+print(Boss)
+while Boss._alive:
+    Boss.take_damage(70)
+    print(Boss)
