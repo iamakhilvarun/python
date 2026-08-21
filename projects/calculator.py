1f36bb1c038343306e1011be68b58093774ee75e
iamakhilvarun <iamakhilvarun@gmail.com>
2026-08-22 04:21:16 +0530

    changes in first project

--- projects/calculator.py
+++ projects/calculator.py
@@ -1,12 +1,9 @@
 # caluclator 
 
 print("Welcome to my first pyhton project")
-menu = ("Addition", "subtraction", "multiplication", "division")
+menu = ["Addition", "subtraction", "multiplication", "division"]
 
-for menu_value in menu:
-    value = menu_value
 
-print(value)
 
 while True:
     print("\n---------MENU ITEMS---------\n")
@@ -26,19 +23,19 @@ while True:
         chossen_input = input("Enter The Operation You Want To Choose:")
 
         if chossen_input == "1":
-            print("you have choosen {} ".format(menu(0)))
+            print("you have choosen {} ".format(menu[0]))
             c = a + b
             print("addtion of {} and {} is : ".format(a, b), c)
         elif chossen_input == "2":
-            print("you have choosen {} ".format(menu(1)))
+            print("you have choosen {} ".format(menu[1]))
             c = a - b
             print("subtraction of {} and {} is : ".format(a, b), c)
         elif chossen_input == "3":
-            print("you have choosen {} ".format(menu(2)))
+            print("you have choosen {} ".format(menu[2]))
             c = a * b
             print("multiplication of {} and {} is : ".format(a, b), c)
         elif chossen_input == "4":
-            print("you have choosen {} ".format(menu(3)))
+            print("you have choosen {} ".format(menu[3]))
             c = a / b
             print("division of {} and {} is : ".format(a, b), c)
         else:
--- projects/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-if "results" in geo_res:
-    lat = geo_res["results"][0]["latitude"]
-    lon = geo_res["results"][0]["longitude"]
-
-    weather_url="https://api.open-meteo.com/v1/forecast"
-    weather_params={
-        "longitude":lon,
-        "latitude":lat,
-    }
